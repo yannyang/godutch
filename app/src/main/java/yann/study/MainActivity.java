d926d88d95e66233468914668ef4f5248570fa06
yann <yannyangguifa@gmail.com>
2015-05-12 01:27:08 +0800

    AA第一次提交

---
+++ app/src/main/java/yann/study/MainActivity.java
@@ -0,0 +1,19 @@
+package yann.study;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
