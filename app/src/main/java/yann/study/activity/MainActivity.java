7044fce8f1b28538805fb0e793be8a1d9fdf147f
Yann <rootka@163.com>
2015-05-12 15:24:11 +0800

    在办公室修改

--- app/src/main/java/yann/study/activity/MainActivity.java
+++ app/src/main/java/yann/study/activity/MainActivity.java
@@ -1,10 +1,12 @@
-package yann.study;
+package yann.study.activity;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
+import android. support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import yann.study.R;
 
 
 public class MainActivity extends Activity {
